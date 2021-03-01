18659106915676879e0649ee70ce4344d28fa1e4
Bran <bbogdanich42@gmail.com>
2021-02-28 23:57:57 -0300

    filtro por categoria y nombre

--- src/views/custom-components/sections/cardCursos.jsx
+++ src/views/custom-components/sections/cardCursos.jsx
@@ -1,15 +1,137 @@
 /* eslint-disable */
 import React from 'react';
 import { Row, Col, Container } from 'reactstrap';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
 
 /**Cursos */
 import {cursos} from '../../components/cursos/cursos';
 
-
-console.log(cursos)
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      color: 'white',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 
 const CardCursos = () => {
+    const classes = useStyles();
+
+    const [state, setState] = React.useState({
+        categoria: '',
+        search: '',
+        name: 'hai',
+        cursosFiltrados: cursos
+    });
+
+
+    const searchFilter = (event) => {
+        
+        setState({
+            ...state,
+            search: event.target.value,
+            cursosFiltrados: filtro(cursos, state.categoria).filter(
+                function (list) 
+                {
+                    return list.titulo.includes(event.target.value)
+                })
+        });
+    
+        
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        setState({
+            ...state,
+            categoria: event.target.value,
+            search: '',
+            [name]: event.target.value,
+            cursosFiltrados: filtro(cursos, event.target.value)
+        });
+    };
+
+    const filtro = (Arr, value) => {
+        if(value === ''){
+            return cursos
+        }else{
+        return Arr.filter(
+            function (list) 
+            {
+                return list.categoria === value
+            })
+        }
+    }
+
     return (
         <div>
             {/* <div className="spacer bg-light">
@@ -28,33 +150,74 @@ const CardCursos = () => {
             <div className="spacer team2">
           
                 <Container>
-                {
-                                cursos.map(({titulo, descripcion}, i) => (
-                    <Row className="m-t-30">
-                        <Col lg="3" md="6" className="m-b-30">
-                                <Col md="12" className="col-md-12 pro-pic t4">
-                            <div className="cards-inline" key={i}>
-                            <div className="card-img-overlay"  >
-                                        <ul className="list-inline list-inlineRedes">
-                                            <li className="list-inline-item"><a href="#"><i className="fa fa-facebook"></i></a></li>
-                                            <li className="list-inline-item"><a href="#"><i className="fa fa-twitter"></i></a></li>
-                                            <li className="list-inline-item"><a href="#"><i className="fa fa-instagram"></i></a></li>
-                                            <li className="list-inline-item"><a href="#"><i className="fa fa-behance"></i></a></li>
-                                        </ul>
-                                        <ul className="list-inline list-inlineinfo">
-                                            <li className="list-inline-item"><a href="#"><h5 className="title font-medium">{titulo}</h5></a></li>
-                                            <li className="list-inline-item"><a href="#"><p>{descripcion}</p></a></li>
-                                            <li className="list-inline-item"><a className="btn btn-info-gradiant btn-md btn-arrow m-t-20  inline-itemBtn" href="#"><span>Más info<i className="ti-arrow-right"></i></span></a></li>
-                                        </ul>
+                    <div className={classes.root}>
+                        <AppBar position="static" style={{backgroundColor:'#01B7E8'}}>
+                            <Toolbar>
+                                <Typography className={classes.title} variant="h6" noWrap>
+                                {state.cursosFiltrados.length?state.cursosFiltrados.length:'0'} Cursos
+                                </Typography>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel className={classes.title} htmlFor="categoria-native-helper">Categoria</InputLabel>
+                                    <NativeSelect
+                                        value={state.categoria}
+                                        onChange={handleChange}
+                                        inputProps={{
+                                            name: 'categoria',
+                                            id: 'categoria-native-helper',
+                                        }}
+                                        > 
+                                            <option aria-label="None" value='' />
+                                            {
+                                                cursos.filter((v,i,a)=>a.findIndex(t=>(t.categoria === v.categoria))===i).map(({categoria}, i) => (
+                                                    <option key={i+'dropdow'} value={categoria}>{categoria}</option>
+                                                ))
+                                            }
+                                    </NativeSelect>
+                                    <FormHelperText className={classes.title}>Seleccione Categoria</FormHelperText>
+                                </FormControl>
+                                <div className={classes.search}>
+                                    <div className={classes.searchIcon}>
+                                    <SearchIcon />
                                     </div>
-                                    
-                            </div>
-                                   
+                                    <InputBase
+                                        placeholder="Search…"
+                                        onChange={searchFilter}
+                                        value={state.search}
+                                        classes={{
+                                            root: classes.inputRoot,
+                                            input: classes.inputInput,
+                                        }}
+                                        inputProps={{ 'aria-label': 'search' }}
+                                    />
+                                </div>
+                            </Toolbar>
+                        </AppBar>
+                    </div>
+                    <Row className="m-t-30">
+                        {
+                            state.cursosFiltrados.map(({titulo, descripcion}, i) => (
+                                <Col lg="3" md="6" className="m-b-30" key={i+'cards'}>
+                                    <Col md="12" className="col-md-12 pro-pic t4">
+                                        <div className="cards-inline">
+                                            <div className="card-img-overlay"  >
+                                                <ul className="list-inline list-inlineRedes">
+                                                    <li className="list-inline-item"><a href="#"><i className="fa fa-facebook"></i></a></li>
+                                                    <li className="list-inline-item"><a href="#"><i className="fa fa-twitter"></i></a></li>
+                                                    <li className="list-inline-item"><a href="#"><i className="fa fa-instagram"></i></a></li>
+                                                    <li className="list-inline-item"><a href="#"><i className="fa fa-behance"></i></a></li>
+                                                </ul>
+                                                <ul className="list-inline list-inlineinfo">
+                                                    <li className="list-inline-item"><a href="#"><h5 className="title font-medium">{titulo}</h5></a></li>
+                                                    <li className="list-inline-item"><a href="#"><p>{descripcion}</p></a></li>
+                                                    <li className="list-inline-item"><a className="btn btn-info-gradiant btn-md btn-arrow m-t-20  inline-itemBtn" href="#"><span>Más info<i className="ti-arrow-right"></i></span></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </Col>
                                 </Col>
-                        </Col>
+                            ))
+                            }
                     </Row>
-                      ))
-                    }
                 </Container>
                 </div> 
                
