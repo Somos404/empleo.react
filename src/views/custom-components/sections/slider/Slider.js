0e31d848116cb7e018f1a0d32ad978922171c9b0
Bran <bbogdanich42@gmail.com>
2021-05-13 16:57:42 -0300

    slider

---
+++ src/views/custom-components/sections/slider/Slider.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Row, Col} from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import customCss from './slider.scss';
+
+const Slidercustom = (props) => {
+    const classes = useStyles();
+    const proppsslider = {
+        autoplay: '10000',
+    }
+    return (
+        <>
+            <div className={classes.preslider}>
+                <h1 className={classes.titleins}> 
+                    Inscripciones abiertas
+                </h1>
+            </div>
+            <Slider {...proppsslider}>
+                {props.content.map((item, index) => (
+                    <div
+                        className={classes.contanerslider}
+                        style={{background: `url(http://localhost:3000/slider/${item.imgUrl}.png)`, backgroundSize: 'cover'}}
+                        key={index+'slider'}
+                    >
+                        <Row className={`${classes.mainContaner} mx-5 center center`}>
+                            <Col className={classes.colContaner}>
+                                <h1 className={classes.title}>
+                                    {item.title}
+                                </h1>
+                                <hr/>
+                            </Col>
+                            <Col className={classes.colContaner}>
+                                <p className={classes.descripcion}>
+                                    {item.descripcionLarga}
+                                </p>
+                            </Col>
+                        </Row>
+                    </div>
+                ))}   
+            </Slider>
+        </>
+    );
+};
+
+const useStyles = makeStyles((theme) => ({
+    preslider:{
+        height: '4em',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#2CB0CF',
+        backgroundImage: 'linear-gradient(to right, #2CB0CF, #7d446e, #a5427c)'
+    },
+    titleins:{
+        marginTop: '.5em',
+        color: 'white', 
+        fontWeight: '900', 
+        fontSize: '1.7rem',
+    },
+    contanerslider:{
+        //background: 'red',
+        height: '15em'
+    },
+    mainContaner:{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '17em'
+    },
+    colContaner:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title:{
+        color: 'white', 
+        fontWeight: '900', 
+        fontSize: '1.5rem', 
+    },
+    descripcion:{
+        color: 'white', 
+        fontSize: '1rem'  
+    }
+   
+}));
+
+export default Slidercustom;
