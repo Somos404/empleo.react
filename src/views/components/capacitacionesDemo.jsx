7c383a6fe58c5e101ceb21dfee1c91428c93327a
Bran <bbogdanich42@gmail.com>
2021-06-17 21:56:00 -0300

    cenit responsive

--- src/views/components/capacitacionesDemo.jsx
+++ src/views/components/capacitacionesDemo.jsx
@@ -122,10 +122,10 @@ const CapacitacionesDemo = (props) => {
                   <Card className="cardsecondContRed">
                     {/* <CardImg top width="100%" src={img1} alt="Card image cap" className="cardImgRed" /> */}
                     <CardBody className="d-flex no-block cardBodyRed">
-                      <Col class="col-md-2">
+                      <Col className="col-md-2">
                         <div className="m-r-20">
                           <img
-                            src={img2}
+                            src={img1}
                             width="55"
                             className="rounded"
                             alt="img"
@@ -154,7 +154,7 @@ const CapacitacionesDemo = (props) => {
                   <Card className="cardsecondContRed">
                     {/* <CardImg top width="100%" src={img1} alt="Card image cap" className="cardImgRed" /> */}
                     <CardBody className="d-flex no-block cardBodyRed">
-                      <Col class="col-md-2">
+                      <Col className="col-md-2">
                         <div className="m-r-20">
                           <img
                             src={img2}
